146a9aaf29c5d23fc659b14a8a9c1d7d13f4430b
hkarim.eth <107819304+khallar@users.noreply.github.com>
2024-08-25 17:43:34 -0300

    Update settings.tsx

--- settings.tsx
+++ settings.tsx
@@ -4,7 +4,7 @@ export const mintText = [
   { label: "WL", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
   { label: "OG", mintText: "Mint Paying with ABC", buttonLabel: "Mint now!", header: "OG Mint" },
   { label: "publi", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" },
-  { label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "Default"}
+  { label: "default", mintText: "Obtene tu NFT Unico --->", buttonLabel: "Mint now!", header: "Default"}
 ];
 
 //header image in the ui. replace with your own
